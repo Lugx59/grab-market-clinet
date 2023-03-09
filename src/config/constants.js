88037d65faf052cda0913e2b61533d49c7a68296
Lugx59 <panzer5959@gmail.com>
2023-03-09 09:58:34 +0900

    api URL 주소 변경

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,3 +1,6 @@
 /* 변수, 상수를 선언하는 곳 */
 
-export const API_URL = 'http://localhost:8080';/* localhost 한개만 익스포트 */
+export const API_URL =
+  process.env.NODE_ENV === 'production'
+    ? 'my-repository-alpha-ebon.vercel.app' :
+    "http:/localhost:8080/";/* localhost 한개만 익스포트 */
